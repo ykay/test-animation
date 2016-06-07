7d2fad33109e442c5a6a1c69ebf1c67e9d7b1539
ykay <ykay@users.noreply.github.com>
2016-06-07 00:41:50 -0700

    Get calendar count

--- test-animation/ViewController.swift
+++ test-animation/ViewController.swift
@@ -13,12 +13,15 @@ class ViewController: UIViewController {
 
     var buttonView: UIView!
     var button: UIButton!
+    var collectionView: UICollectionView!
     
     let diameter: CGFloat = 100
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.whiteColor()
         
         buttonView = SpringView()
         buttonView.frame = CGRectMake(300, 300, diameter, diameter)
@@ -27,21 +30,26 @@ class ViewController: UIViewController {
         buttonView.layer.borderColor = UIColor.blueColor().CGColor
         buttonView.layer.cornerRadius = buttonView.frame.size.height / 2
         view.addSubview(buttonView)
+
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(onButtonTap))
+        buttonView.addGestureRecognizer(recognizer)
         
-//        let buttonHoldRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(onButtonHold))
-//        buttonHoldRecognizer.minimumPressDuration = 0.8
-        let buttonHoldRecognizer = UITapGestureRecognizer(target: self, action: #selector(onButtonHold))
-        buttonView.addGestureRecognizer(buttonHoldRecognizer)
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .Horizontal
+        collectionView = UICollectionView(frame: CGRectMake(0, 0, 300, 100), collectionViewLayout: layout)
+//        collectionView.delegate = self
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
-    func onButtonHold() {
+    func onButtonTap() {
         UIView.animateWithDuration(0.4, delay: 0.0, usingSpringWithDamping: 0.4, initialSpringVelocity: 1.0, options: UIViewAnimationOptions.TransitionNone, animations: {
-            
                 if self.buttonView.transform.d == 0.5 {
                     let transform = CGAffineTransformScale(CGAffineTransformIdentity, 1.0, 1.0)
                     self.buttonView.transform = transform
@@ -50,8 +58,43 @@ class ViewController: UIViewController {
                     self.buttonView.transform = transform
                 }
             }) { (success) in
-                
         }
     }
 }
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let today = NSDate()
+        let range = NSCalendar.currentCalendar().rangeOfUnit(.Day, inUnit: .Month, forDate: today)
+        let numDays = range.length
+        
+        let dateComponents = NSDateComponents()
+        dateComponents.year = NSCalendar.currentCalendar().component(.Year, fromDate: today)
+        dateComponents.month = NSCalendar.currentCalendar().component(.Month, fromDate: today)
+        dateComponents.day = 1
+        let firstDay = NSCalendar.currentCalendar().dateFromComponents(dateComponents)
+        
+        let weekdayOfFirstDay = NSCalendar.currentCalendar().component(.Weekday, fromDate: firstDay!)
+        let extraPreMonthDays = 7 - (7 - (weekdayOfFirstDay - 1))
+        
+        let lastDayComponents = NSDateComponents()
+        lastDayComponents.year = NSCalendar.currentCalendar().component(.Year, fromDate: today)
+        lastDayComponents.month = NSCalendar.currentCalendar().component(.Month, fromDate: today)
+        lastDayComponents.day = numDays
+        let lastDay = NSCalendar.currentCalendar().dateFromComponents(lastDayComponents)
+        
+        let weekdayOfLastDay = NSCalendar.currentCalendar().component(.Weekday, fromDate: lastDay!)
+        let extraPostMonthDays = 7 - weekdayOfLastDay
+        
+        return numDays + extraPreMonthDays + extraPostMonthDays
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+//let weekday = NSCalendar.currentCalendar().component(.Weekday, fromDate: NSDate())
+        return UICollectionViewCell()
+    }
+}
+//extension ViewController: UICollectionViewDelegate {
+//    collection
+//}
 
